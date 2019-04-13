b89a6a1c74d1dface4fbdfee13a744b30f865e29
Nikita <nikita.kalyaev@gmail.com>
2019-04-13 03:56:07 +0300

    2

--- js/third.js
+++ js/third.js
@@ -1,8 +1,23 @@
+imgs = new Array();
+
+//createField();
+
+
+
 var 
-	canvas = document.createElement('canvas');
-	body = document.getElementById('body');
-	div = document.createElement('div');
+	canvas = document.createElement('canvas'),
+	body = document.getElementById('body'),
+	div = document.createElement('div'),
 	save = document.createElement('button');
+for (var i = 0; i < 4; i++){
+    imgs[i] = new Image();
+    imgs[i].crossOrigin="anonymous";
+ }
+
+var
+ countLoadImgs = 0,
+ countDrawImgs;
+console.log(countLoadImgs);
 
 canvas.id = 'canvas';
 save.id = 'save';
@@ -26,12 +41,69 @@ save.style.margin = '20px 250px 0 250px';
 //			URL = c.toDataURL("");
 			//ДОДЕЛАТЬ ФУНКЦИЮ
 //	}
+//getPic();
+get();
+draw();
 
 // getPic
+function getPic(){
 var xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://api.unsplash.com/photos/random', false, '93d496ca0d56955281ca9a8ec2b1a87610bdc2ff60d2edbbe38982ffc123ac2d');
+xhr.open('GET', 'https://api.codetabs.com/v1/proxy?' + 'quest=https://api.unsplash.com/photos/random?' + 'client_id=93d496ca0d56955281ca9a8ec2b1a87610bdc2ff60d2edbbe38982ffc123ac2d&count=4', false);
 xhr.send();
-alert(xhr.responseText);
+var json = JSON.parse(xhr.responseText);
+//alert(json.urls.small);
+for (var i = 0; i < 4; i++) {
+imgs[i] = json[i].urls.small;
+//alert(imgs[i]);
+
+}
+}
+
+function get(){
+  $.ajax({
+    url: "https://api.codetabs.com/v1/proxy",
+    data: {
+      quest : 'https://api.unsplash.com/photos/random?client_id=1bf5ac4b983dada224863bfb469103d7b2d8577fc38908b5fe2813c835a66d6c&count=4&orentation=squarish&w=300&h=300'
+    }
+  })
+  .done(
+    function(data) {
+      for (var i = 0; i < 4; i++) {
+        imgs[i].src = data[i].urls.custom;
+        imgs[i].onload = function(){
+        countLoadImgs++;
+        //alert(countLoadImgs);
+        };
+      };
+   })
+}
+
+
+
+function draw(){
+if (countLoadImgs == 4) {
+ var ctx = canvas.getContext('2d');
+
+
+ ctx.drawImage(imgs[0],0,0,300,300,0,0,75,35.5);
+ ctx.drawImage(imgs[1],0,0,300,300,150,0,300,150);
+ ctx.drawImage(imgs[2],0,0,300,300,0,75,300,150);
+ ctx.drawImage(imgs[3],0,0,300,300,150,75,300,150);
+ ctx.fillStyle = "rgba(0,0,0,0.35)";
+ ctx.fillRect(0,0,600,600);
+
+
+  countDrawImgs++;
+   } else {
+        setTimeout(draw, 1);
+    }
+  
+}
+
+
+
+
+
 
 
 div.appendChild(canvas);
